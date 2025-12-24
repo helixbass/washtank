590e244d0ee4193c3483ef8a60b54de913598d5d
Julian Rosse <julian@helixbass.net>
2025-12-24 10:45:20 -0500

    working on rope

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use clap::Parser;
+use crossterm::{
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use ropey::Rope;
+use squalid::_d;
+use tokio::fs;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    enable_raw_mode()?;
+    let mut stdout = stdout();
+    execute!(stdout, EnterAlternateScreen)?;
+
+    Editor::default().run().await?;
+
+    execute!(stdout, LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
+}
+
+#[derive(Parser)]
+struct Args {
+    pub file_name: PathBuf,
+}
+
+#[derive(Default)]
+pub struct Editor {
+    pub current_file: OpenFile,
+}
+
+impl Editor {
+    async fn run(&mut self) -> Result<(), anyhow::Error> {
+        let args = Args::parse();
+
+        self.open_file(args.file_name)?;
+    }
+
+    async fn open_file(&mut self, file_name: PathBuf) -> Result<(), anyhow::Error> {
+        let rope = Rope::from_str(&fs::read_to_string(&file_name).await?);
+        self.current_file = OpenFile::Named(OpenFileNamed {
+            rope,
+            path: file_name,
+        });
+
+        unimplemented!();
+        Ok(())
+    }
+}
+
+pub enum OpenFile {
+    Anonymous(OpenFileAnonymous),
+    Named(OpenFileNamed),
+}
+
+impl Default for OpenFile {
+    fn default() -> Self {
+        Self::Anonymous(_d())
+    }
+}
+
+#[derive(Default)]
+pub struct OpenFileAnonymous {
+    pub rope: Rope,
+}
+
+pub struct OpenFileNamed {
+    pub rope: Rope,
+    pub path: PathBuf,
 }
